b46bf4087cdd460285f7c44ff66a9306cc5def55
HAmid <hamidprince823@gmail.com>
2023-10-23 12:15:37 +0500

    debug

--- src/app/api/pets/route.ts
+++ src/app/api/pets/route.ts
@@ -1,10 +1,9 @@
 import { sql } from '@vercel/postgres';
 import { NextRequest, NextResponse } from 'next/server';
-// import { db } from '@vercel/postgres';
-import { db, table } from '@/app/lib/drizzel';
+ import { db, table } from '@/app/lib/drizzel';
 
 
-export async function GET(request: NextRequest) {
+export async function GET(request:  Request) {
     const data = await db.select().from(table);
 
     try {
@@ -13,13 +12,4 @@ export async function GET(request: NextRequest) {
         return NextResponse.json("Check Your Network");
     }
 }
-export async function Post(request: Request) {
-    const body = await request.json()
-    await db.insert(table).values(body);
-    try {
-        return console.log(body, "body");
-    } catch (error) {
-        return console.log(error);
-    }
-    console.log(body);
-}
+   
